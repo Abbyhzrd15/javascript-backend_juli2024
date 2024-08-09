6abbbcba86816d0913ad4fc446cc71c489b74537
Abbyhzrd15 <abbydrajad15@gmail.com>
2024-08-09 19:43:54 +0700

    basic routing

---
+++ app-express.js
@@ -0,0 +1,15 @@
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World!</h1>')
+})
+//route -> rute
+app.get('/hubungi', function (req, res) {
+  res.send('<h1>Silakan Wa: 081273257899</h1>')
+})
+
+app.listen(port, () => {
+  console.log('Server sudah siap, buka http://localhost:' + port)
+})
