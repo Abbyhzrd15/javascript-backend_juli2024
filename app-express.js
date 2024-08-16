86ed249b65191ce06fb90625318c66ab6b387211
Abbyhzrd15 <abbydrajad15@gmail.com>
2024-08-16 21:49:19 +0700

    membuat controller biar rapih

--- app-express.js
+++ app-express.js
@@ -27,6 +27,8 @@ const m_karyawan    = require('./model/m_karyawan')
 const m_departemen  = require('./model/m_departemen')
 const m_agama       = require('./model/m_agama')
 
+const c_karyawan = require('./controller/c_karyawan')
+
 //route -> rute
 app.get('/hubungi', function (req, res) {
   let data = {
@@ -45,88 +47,13 @@ app.get('/profil', function (req, res) {
     res.render('profil-developer', data)
 })
 
-app.get('/karyawan', async function (req,res) {  
-    let dataview = {
-      karyawan: await m_karyawan.get_semuaKaryawan(),
-      message: req.query.msg,
-  }
-  res.render('karyawan/index', dataview)
-})
-
-app.get('/karyawan/detail/:id_karyawan', async function (req,res) {  
-//ambil id yg dikirim via url 
-  let idk = req.params.id_karyawan
-
-  //setelah itu kirim ke proses request data mysql
-  let dataview = {
-      pegawai: await m_karyawan.get_satuKaryawan(idk),
-  }
-    res.render('karyawan/detail', dataview)
-})
-
-app.get('/karyawan/hapus/:id_karyawan', async function (req,res) {
-  //ambil id yg dikirim via url 
-  let idk = req.params.id_karyawan
-
-  //proses hapus data
-  try {
-    let hapus = await m_karyawan.hapus_satuKaryawan(idk)
-    if (hapus.affectedRows > 0) {
-      res.redirect(`/karyawan?msg=berhasil hapus karyawan`)
-    }
-  } catch (error) {
-    throw error
-  }
-})
-
-app.get('/karyawan/tambah', async function (req,res) {
-  //ambil data departemen dari database tabel departemen
-  let dataview = {
-    dept: await m_departemen.get_semuaDepartemen(),
-    agm: await m_agama.get_semuaAgama(),
-  }
-  res.render('karyawan/form-tambah', dataview)
-
-})
-
-app.post('/karyawan/proses-insert', async function (req,res) {
-  //terima kiriman data dari form html
-  // let body = req.body
-
-  try {
-    let insert = await m_karyawan.insert_karyawan(req)
-    if (insert.affectedRows > 0) {
-      res.redirect(`/karyawan?msg=berhasil tambah karyawan a/n ${req.body.form_nama_lengkap}`)
-    }
-  } catch (error) {
-    throw error
-  }
-})
-
-app.get('/karyawan/edit/:id_karyawan', async function (req,res) {
-  let idk = req.params.id_karyawan
-  let dataview = {
-    dept    : await m_departemen.get_semuaDepartemen(),
-    agm     : await m_agama.get_semuaAgama(),
-    pegawai : await m_karyawan.get_satuKaryawan(idk),
-  }
-  res.render('karyawan/form-edit', dataview)
-})
-
-app.post('/karyawan/proses-update/:id_karyawan' , async function (req, res) {
-  let idk = req.params.id_karyawan
-  try {
-    let update = await m_karyawan.update_karyawan(req, idk)
-    if (update.affectedRows > 0) {
-      res.redirect(`/karyawan?msg=berhasil edit karyawan a/n ${req.body.form_nama_lengkap}`)
-    }
-  } catch (error) {
-    throw error
-  }
-})
-
-
-
+app.get('/karyawan', c_karyawan.index)
+app.get('/karyawan/detail/:id_karyawan', c_karyawan.detail)
+app.get('/karyawan/hapus/:id_karyawan', c_karyawan.hapus)
+app.get('/karyawan/tambah', c_karyawan.tambah)
+app.post('/karyawan/proses-insert', c_karyawan.proses_insert)
+app.get('/karyawan/edit/:id_karyawan', c_karyawan.proses_edit)
+app.post('/karyawan/proses-update/:id_karyawan' , c_karyawan.proses_update)
 
 app.listen(port, function () {
   console.log('Server sudah siap, buka http://localhost:' + port)
